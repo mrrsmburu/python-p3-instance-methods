f30a91d54a3f1b7caebb3ccf0ed2feab4f6aeb5c
mrrsmburu <mrrsmburu@gmail.com>
2023-12-16 11:48:52 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(f"{self.name} is barking.")
+
+    def sit(self):
+        print(f"{self.name} is sitting.")
+def bark(self):
+        print("Woof!")
+
+my_dog = Dog(name="Buddy", age=3)
+
+my_dog.bark()
+
+my_dog.sit()
+
+
+
+   
+
--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,16 @@
 class Person:
     # Class body goes here
 
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print("Hello, my name is", self.name)
+
+    def talk(self):
+        print("Hello World!")
+
     #Instance method definition
-    pass
+        person = Person("John")
+        person.greet()  # Output: Hello, my name is John
+        person.talk()
